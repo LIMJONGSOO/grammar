83be1265e90b81f30ad6eb9f0cb8c5fa86a00574
LIMJONGSOO <limjs4535@gmail.com>
2020-07-17 23:34:56 +0900

    js

--- javascript/es5.js
+++ javascript/es5.js
@@ -1,60 +1,62 @@
 // javasript 특성
 // 인터프리터 언어
 // 이진 부동소수점 숫자를 사용
-var x = 0.2 + 0.1;// 0.30000000000000004
+var x = 0.2 + 0.1; // 0.30000000000000004
 // 함수 유효범위
 // 함수내 변수는 호스팅되어 자동으로 함수 맨 위에서 선언됨
 
-
 var myNumber = 32;
-myNumber.toString(10);  // returns 32 10진법
-myNumber.toString(32);  // returns 10 32진법
-myNumber.toString(16);  // returns 20 16진법
-myNumber.toString(8);   // returns 40 8진법
-myNumber.toString(2);   // returns 100000 2진법
+myNumber.toString(10); // returns 32 10진법
+myNumber.toString(32); // returns 10 32진법
+myNumber.toString(16); // returns 20 16진법
+myNumber.toString(8); // returns 40 8진법
+myNumber.toString(2); // returns 100000 2진법
 
 // Math 함수
-Math.pow(2,4) // 2의 4승 2**4
-Math.round(.6) //반올림
-Math.ceil(.6) //올림
-Math.floor(.6)//버림
-Math.abs(-5) // 절대 값
-Math.max(1,2,3) // 최대값
-Math.min(1,2,3) // 최소값
-Math.random() // 0과 1사이의 임의의 수
-Math.PI // 원주율
-Math.E // 자연로그상수
-Math.sqrt(64) // 8 의 제곱근
+Math.pow(2, 4); // 2의 4승 2**4
+Math.round(0.6); //반올림
+Math.ceil(0.6); //올림
+Math.floor(0.6); //버림
+Math.abs(-5); // 절대 값
+Math.max(1, 2, 3); // 최대값
+Math.min(1, 2, 3); // 최소값
+Math.random(); // 0과 1사이의 임의의 수
+Math.PI; // 원주율
+Math.E; // 자연로그상수
+Math.sqrt(64); // 8 의 제곱근
 
-var f = function(x) { return x;} //변수에 함수 할당
-function f(x) {return x;} // 함수선언
+var f = function (x) {
+  return x;
+}; //변수에 함수 할당
+function f(x) {
+  return x;
+} // 함수선언
 
-switch(n) {
-    case 1:
-        break;
-    case 2:
-        break;
-    default:
-        break;
+switch (n) {
+  case 1:
+    break;
+  case 2:
+    break;
+  default:
+    break;
 }
 
 // 점프문
 //label 레이블
-mainloop: while(token != null) {
-    continue mainloop; //주어진 mainloop의 다음 반복으로 이동
+mainloop: while (token != null) {
+  continue mainloop; //주어진 mainloop의 다음 반복으로 이동
 }
 
 // object 객체
-var o = {a: 1};
-var p = inherit(0);  // 객체 o를 상속
+var o = { a: 1 };
+var p = inherit(0); // 객체 o를 상속
 delete o.a; //a프로퍼티 삭제
 a in o; // o 객제에 a 프로퍼티 여부
-
 
 // array 배열
 var a = new Array();
 var a2 = new Array(10);
-var a3 = new Array(1,2,3,4);
+var a3 = new Array(1, 2, 3, 4);
 
 a3.join(); //'1,2,3,4' 모든 원소를 문자열로 변환하여 붙인 결과
 a3.join(""); // '1234'
@@ -62,9 +64,9 @@ a3.reverse(); // 순서반대로
 a.sort(); // 인자값이 없으면 알파벳순
 a.concat(a2); // a + a2 배열반환
 a3.slice(3); //[4]
-a3.slice(0,1); //[1]
+a3.slice(0, 1); //[1]
 a3.splice(3); //[4], a3 = [1,2,3]
-a3.splice(1,1 ,'a','b'); // [2], a3=[1,'a','b',3]
+a3.splice(1, 1, "a", "b"); // [2], a3=[1,'a','b',3]
 //push, pop 배열 맨 끝에 추가 삭제
 //unshift, shift 배열 맨 앞에 추가 삭제
 //array function
